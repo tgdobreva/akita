dfca636f38ca9b5c668243d8c285435af25a2da1
Teodora Dobreva <teodora.dobreva@documaster.com>
2021-06-21 21:45:33 +0300

    Replace system logging with slf4j

--- src/main/java/com/github/tgdobreva/akita/Akita.java
+++ src/main/java/com/github/tgdobreva/akita/Akita.java
@@ -1,16 +1,20 @@
 package com.github.tgdobreva.akita;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Akita {
 
+	private static final Logger LOG = LoggerFactory.getLogger(Akita.class);
+
 	public static void main(String[] args) throws Exception {
 		try {
 			SpringApplication.run(Akita.class, args);
 		} catch (Exception ex) {
-			System.err.println("Could not start Akita");
+			LOG.error("Could not start Akita", ex);
 			System.exit(1);
 		}
 	}
